2f8090cc7ea8feef19bc52f6a8f5ad10a2110676
Fazlul <fkarif.cse@gmail.com>
2020-01-28 23:06:56 +0100

    vuejs quote list app final production

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <app-header :maxQuote="maxQuote" :quoteLength="quotes.length"></app-header>
+    <app-new-quote @addQuoteItem="addNewQuote"></app-new-quote>
+    <app-quote-grid :quoteListItems="quotes" @deleteQuote="deleteQuote"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import quoteGrid from "@/components/quoteGrid.vue";
+import newQuote from "@/components/newQuote.vue";
+import Header from "@/components/Header.vue";
+export default {
+  data() {
+    return {
+      quotes: ["This is forst Quote"],
+      maxQuote: 3
+    };
+  },
+  components: {
+    "app-quote-grid": quoteGrid,
+    "app-new-quote": newQuote,
+    "app-header": Header
+  },
+  methods: {
+    newQuote() {
+      this.quotes = [];
+    },
+    addNewQuote(quoteItem) {
+      if (this.quotes.length >= this.maxQuote) {
+        if (confirm("You quote list ended!! Start New Quote List")) {
+          this.newQuote();
+        }
+      }
+      this.quotes.push(quoteItem);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/quoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-bind:key="quote"
+      v-for="(quote, index) in quoteListItems"
+      @click.native="deleteQuote(index)"
+    >{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import quote from "@/components/quote.vue";
+export default {
+  props: ["quoteListItems", "maxQuote", "quoteLength"],
+  components: {
+    "app-quote": quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("deleteQuote", index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
